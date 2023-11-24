7274c33052fac40c5464e6ca6e93df88a12c9dde
Norbert Fuksz <norbert.fuksz@sectigo.com>
2023-11-24 08:58:31 +0000

    Update examples

--- examples/requests_get.rb
+++ examples/requests_get.rb
@@ -1,6 +1,7 @@
 require_relative "../lib/xolphin/api"
+require_relative "_credentials"
 
-client = Xolphin::Api::Client.new(Credentials.username, Credentials.password, true)
+client = Xolphin::Api::Client.new(Credentials.username, Credentials.password, false)
 requests = client.request.all
 requests.each do |request|
   puts request.id, request.product.id
